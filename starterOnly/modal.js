20dccae4a1e46cfd34dd015aceb3a0126728bece
Howthamaraj MOHANARAJ <howtha.mohanaraj@gmail.com>
2022-01-06 23:53:57 +0100

    done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,6 +19,15 @@ var email = document.getElementById("email");
 var quantity = document.getElementById("quantity");
 var locationCity = document.getElementsByName("location");
 var checkBox1 = document.getElementById("checkbox1");
+var birthdate = document.getElementById("birthdate");
+
+var firstNameErr = document.getElementById("firstErr");
+var lastNameErr = document.getElementById("lastErr");
+var emailErr = document.getElementById("emailErr");
+var quantityErr = document.getElementById("quantityErr");
+var locationErr = document.getElementById("locationErr");
+var checkbox1Err = document.getElementById("checkbox1Err");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -43,34 +52,45 @@ function validate()
   if (firstName.value == "" || firstName.value.length < 2)
   {
     //alert("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
-    firstName.innerHTML = "<br> Veuillez saisir !"
+    firstNameErr.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
     return false;
   }
+  firstNameErr.style.display = "none";
   if (lastName.value == "" || lastName.value.length < 2)
   {
-    alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    //alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    lastNameErr.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
     return false;
   }
+  lastNameErr.style.display = "none";
   if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))
   {
-    alert("Veuillez saisir une adresse email valide !");
+    //alert("Veuillez saisir une adresse email valide !");
+    emailErr.innerHTML = "Veuillez saisir une adresse email valide !"
     return false;
   }
+  emailErr.style.display = "none";
   if (!quantity.value.match(/^([0-9]){1,2}$/)) //chiffre répété entre 1 & 2 fois
   {
-    alert("Veuillez saisir une valeur numérique !");
+    //alert("Veuillez saisir une valeur numérique !");
+    quantityErr.innerHTML = "Veuillez saisir une valeur numérique !"
     return false;
   }
+  quantityErr.style.display = "none";
   if (!(locationCity[0].checked || locationCity[1].checked || locationCity[2].checked || locationCity[3].checked || locationCity[4].checked || locationCity[5].checked))
   {
-    alert("Vous devez choisir une option.")
+    //alert("Vous devez choisir une option.")
+    locationErr.innerHTML = "Vous devez choisir une option."
     return false;
   }
+  locationErr.style.display = "none";
   if (!checkBox1.checked)
   {
-    alert("Vous devez vérifier que vous acceptez les termes et conditions.");
+    //alert("Vous devez vérifier que vous acceptez les termes et conditions.");
+    checkbox1Err.innerHTML = "Vous devez choisir une option."
     return false;
   }
+  checkbox1Err.style.display = "none";
   alert("Merci ! Votre réservation a été reçue.");
   return true;
 }
